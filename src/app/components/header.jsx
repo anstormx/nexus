3dfbf47e4a3040925abcd3d69aa328f174add4fc
Aniket <aniketkini1@gmail.com>
2024-07-22 02:05:17 +0530

    logo changes

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -1,4 +1,4 @@
-import Logo from "../logo.png";
+import Logo from "../favicon.ico";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,18 +8,18 @@ function Header() {
     <div className="w-full flex justify-between items-center px-[2%] py-[1%]">
       <div className="flex flex-row items-center text-gray-400">
         <Image src={Logo} alt="logo" className="h-12 w-12"/>
-        <div className="font-bold text-2xl ml-[2%] text-pink-500">
-          nexus
+        <div className="font-bold text-2xl ml-[3%] text-pink-500">
+          NEXUS
         </div>
-        <div className="ml-8 hover:text-gray-300 transition duration-200 mt-1">
+        <div className="ml-8 hover:text-gray-300 transition duration-200">
           <Link href="/">
             Trade
           </Link>
         </div>
-        <div className="ml-6 hover:text-gray-300 transition duration-200 cursor-pointer mt-1">
+        <div className="ml-6 hover:text-gray-300 transition duration-200 cursor-pointer">
           Explore
         </div>
-        <div className="ml-6 hover:text-gray-300 transition duration-200 cursor-pointer mt-1">
+        <div className="ml-6 hover:text-gray-300 transition duration-200 cursor-pointer">
           Pool
         </div>
       </div>
