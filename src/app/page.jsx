d2aed64683a06daf6fb840f26a0b4f1d770009bb
Aniket <aniketkini1@gmail.com>
2024-07-29 00:06:11 +0530

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,12 +16,14 @@ import { Pagination, Autoplay } from "swiper/modules";
 export default function Home() {
   const [totalLiquidity, setTotalLiquidity] = useState("0");
   const [totalVolume, setTotalVolume] = useState("0");
+  const [totalUsers, setTotalUsers] = useState("0");
   const isMounted = useIsMounted();
 
   useEffect(() => {
     if (isMounted) {
       let prevLiquidity = 19563920000; // Starting value for liquidity
       let prevVolume = 5176342000; // Starting value for volume
+      let prevUsers = 12930; // Starting value for users
 
       const updateValues = () => {
         const liquidityIncrement = Math.floor(Math.random() * 9000) + 1000;
@@ -30,16 +32,23 @@ export default function Home() {
         const volumeIncrement = Math.floor(Math.random() * 4500) + 5000;
         prevVolume += volumeIncrement;
 
+        const usersIncrement = Math.floor(Math.random() * 10) + 10;
+        prevUsers += usersIncrement;
+
         setTotalLiquidity(
           `$${prevLiquidity.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`
         );
         setTotalVolume(
           `$${prevVolume.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`
         );
+
+        setTotalUsers(
+          `${prevUsers.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`
+        );
       };
 
       updateValues();
-      const interval = setInterval(updateValues, 2000);
+      const interval = setInterval(updateValues, 1500);
 
       return () => clearInterval(interval);
     }
@@ -50,48 +59,44 @@ export default function Home() {
     <div>
       <Header />
       <main className="container mx-auto px-4 py-8 max-w-4xl">
-        <section className="text-center mb-16 font-semibold">
+        <section className="text-center mb-8 font-semibold">
           {/* <h1 className="text-6xl font-bold mb-4">Welcome to NEXUS</h1> */}
           <p className="text-6xl mb-12 font-semibold">
-            Build on the leading decentralized crypto trading protocol
+            Shape the future on the leading decentralized crypto trading
+            protocol
           </p>
-          <div className="flex justify-center gap-6">
-            <Link
-              href="/trade"
-              className="bg-pink-500 hover:bg-pink-600 font-bold py-3 px-6 rounded-lg transition duration-300"
-            >
-              Start Trading
-            </Link>
-            <Link
-              href="/pool"
-              className="bg-zinc-600 hover:bg-zinc-700 font-bold py-3 px-6 rounded-lg transition duration-300"
-            >
-              Add Liquidity
-            </Link>
-          </div>
         </section>
 
-        <section className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8 px-[8%]">
-          <div className="bg-zinc-800 p-6 rounded-lg">
-            <h2 className="text-2xl font-semibold mb-4">Total Liquidity</h2>
-            <p className="text-4xl font-bold text-pink-500">{totalLiquidity}</p>
-          </div>
-          <div className="bg-zinc-800 p-6 rounded-lg">
-            <h2 className="text-2xl font-semibold mb-4">24h Trading Volume</h2>
-            <p className="text-4xl font-bold text-pink-500">{totalVolume}</p>
+        <section className="mb-16 w-full">
+          <div className="flex justify-between items-center bg-transparent p-6 rounded-lg">
+            <div className="text-center">
+              <p className="text-2xl font-semibold mb-2">Total Liquidity</p>
+              <p className="text-4xl font-bold text-pink-500">
+                {totalLiquidity}
+              </p>
+            </div>
+            <div className="text-center">
+              <p className="text-2xl font-semibold mb-2">Total Users</p>
+              <p className="text-4xl font-bold text-pink-500">{totalUsers}</p>
+            </div>
+            <div className="text-center">
+              <p className="text-2xl font-semibold mb-2">24h Trading Volume</p>
+              <p className="text-4xl font-bold text-pink-500">{totalVolume}</p>
+            </div>
           </div>
         </section>
 
         <section>
           <Swiper
             slidesPerView={1}
-            spaceBetween={30}
             autoplay={{
-              delay: 2000,
+              delay: 3000,
               disableOnInteraction: false,
             }}
             loop={true}
             modules={[Pagination, Autoplay]}
+            centeredSlides={true}
+            speed={1000}
           >
             <SwiperSlide>
               <FeatureCard
@@ -123,19 +128,19 @@ export default function Home() {
 
 function FeatureCard({ title, description, icon }) {
   return (
-    <div className="bg-zinc-600 p-6 mx-[30%] rounded-lg flex items-center h-full">
+    <div className="bg-zinc-600 p-6 mx-[30%] rounded-2xl flex items-center h-full">
       <div className="mr-6">
         <Image
           src={icon}
           alt={title}
-          width={120}
-          height={120}
+          width={300}
+          height={300}
           className="mb-4"
         />
       </div>
       <div>
-        <h3 className="text-xl font-semibold mb-2">{title}</h3>
-        <p className="text-gray-300">{description}</p>
+        <h3 className="text-2xl font-semibold mb-2">{title}</h3>
+        <p className="text-base">{description}</p>
       </div>
     </div>
   );
